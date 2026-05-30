8b48678bee6bc61d11150205de9641276d5c12ab
En <irene.fernanda@ti.ukdw.ac.id>
2026-05-30 17:14:43 +0700

    managerrr

---
+++ src/main/java/utils/SessionManager.java
@@ -0,0 +1,12 @@
+package utils;
+
+import model.User;
+
+public class SessionManager {
+    private static User currentUser;
+
+    public static User getCurrentUser() { return currentUser; }
+    public static void setCurrentUser(User user) { currentUser = user; }
+    public static void clear() { currentUser = null; }
+    public static boolean isLoggedIn() { return currentUser != null; }
+}
